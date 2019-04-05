49e15679b9e9b473745ddbe44ac8be64e5d150c3
Yiheng (NUC) <development.henryquan@gmail.com>
2019-04-05 19:28:11 +1100

    create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './page/detail.dart';
-import './value/colour.dart';
+import './page/home.dart';
 
 void main() => runApp(MC());
 
@@ -12,58 +11,7 @@ class MC extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Material Colour'),
+      home: HomePage(title: 'Material Colour'),
     );
   }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  Color hexToColor(String code) {
-    print(code);
-    return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: OrientationBuilder(
-        builder: (c, o) {
-          return GridView.count(
-            crossAxisCount: o == Orientation.portrait ? 2 : 4,
-            children: List.generate(FlutterMaterialColour.length, (index) {
-              var currItem = FlutterMaterialColour.values.elementAt(index);
-              var currColour = FlutterMaterialColour.keys.elementAt(index);
-              return Padding(
-                child: Material(
-                  shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16) ),
-                  elevation: 4,
-                  clipBehavior: Clip.antiAlias,
-                  child: FlatButton(
-                    child: null,
-                    color: hexToColor(currItem['500']),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ColourPage(title: currColour, colour: currItem)),
-                      );
-                    },
-                  ),
-                ),
-                padding: EdgeInsets.only(left: 8, right: 8, top: 16, bottom: 8),
-              );
-            }),
-          );
-        },
-      )
-    );
-  }
-
-  
 }
---
+++ lib/page/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import './detail.dart';
+import '../value/colour.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  Color hexToColor(String code) {
+    print(code);
+    return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: OrientationBuilder(
+        builder: (c, o) {
+          return GridView.count(
+            crossAxisCount: o == Orientation.portrait ? 2 : 4,
+            children: List.generate(FlutterMaterialColour.length, (index) {
+              var currItem = FlutterMaterialColour.values.elementAt(index);
+              var currColour = FlutterMaterialColour.keys.elementAt(index);
+              return Padding(
+                child: Material(
+                  shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16) ),
+                  elevation: 4,
+                  clipBehavior: Clip.antiAlias,
+                  child: FlatButton(
+                    child: null,
+                    color: hexToColor(currItem['500']),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ColourPage(title: currColour, colour: currItem)),
+                      );
+                    },
+                  ),
+                ),
+                padding: EdgeInsets.only(left: 8, right: 8, top: 16, bottom: 8),
+              );
+            }),
+          );
+        },
+      )
+    );
+  }
+
+  
+}
