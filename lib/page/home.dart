28a399964140764af8af85ead19a23e8fb73949d
Yiheng (Mac) <development.henryquan@gmail.com>
2019-05-19 20:19:48 +1000

    adjust margin and padding

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -29,71 +29,62 @@ class HomePage extends StatelessWidget {
     ColourNames.forEach((n) => list.add(new MaterialColour(n)));
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black87
+          ),
+        ),
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        actions: <Widget>[
+          IconButton(
+            color: Colors.black87,
+            icon: Icon(Icons.info_outline),
+            tooltip: 'About Material Colour',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AboutPage()),
+              );
+            },
+          )
+        ],
+      ),
       body: LayoutBuilder(
         builder: (c, constraint) {
           // At least 2 items, at most 7 items
           var itemNum = max(2, min((constraint.maxWidth / 200).floor(), 7));
 
-          return CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                floating: true,
-                expandedHeight: 44,
-                flexibleSpace: AppBar(
-                  title: Text(
-                    title,
-                    style: TextStyle(
-                      color: Colors.black87
+          return Container(
+            padding: EdgeInsets.only(left: 8, right: 8),
+            child: GridView.count(
+              crossAxisCount: itemNum,
+              children: list.map((mc) {
+                return Padding(
+                  padding: EdgeInsets.only(left: 8, right: 8, top: 8, bottom: 8),
+                  child: Material(
+                    shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16)),
+                    elevation: 4,
+                    clipBehavior: Clip.antiAlias,
+                    child: FlatButton(
+                      child: null,
+                      color: mc.mainColour.colour,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ColourPage(title: mc.name, mc: mc)),
+                        );
+                      },
                     ),
                   ),
-                  backgroundColor: Colors.white,
-                  brightness: Brightness.light,
-                ),
-                actions: <Widget>[
-                  IconButton(
-                    color: Colors.black87,
-                    icon: Icon(Icons.info_outline),
-                    tooltip: 'About Material Colour',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AboutPage()),
-                      );
-                    },
-                  )
-                ],
-              ),
-              SliverGrid.count(
-                crossAxisCount: itemNum,
-                children: list.map((mc) {
-                  return Padding(
-                    child: Material(
-                      shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(16)),
-                      elevation: 4,
-                      clipBehavior: Clip.antiAlias,
-                      child: FlatButton(
-                        child: null,
-                        color: mc.mainColour.colour,
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ColourPage(title: mc.name, mc: mc)),
-                          );
-                        },
-                      ),
-                    ),
-                    padding: EdgeInsets.only(left: 8, right: 8, top: 16, bottom: 8),
-                  );
-                }).toList(),
-              ),
-            ],
+                );
+              }).toList(),
+            ),
           );
         },
       ),
     );
   }
 }
-
-
-
-        
