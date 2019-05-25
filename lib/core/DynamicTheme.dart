0ced0639d5a46a995001aa219b8ee1c90a1bff97
Yiheng (NUC) <development.henryquan@gmail.com>
2019-05-25 11:37:37 +1000

    add FlutterTheme widget, its state and InheritedWidget

--- lib/core/DynamicTheme.dart
+++ lib/core/DynamicTheme.dart
@@ -5,8 +5,6 @@ Theme class for dynamic theming
 import 'package:flutter/material.dart';
 
 class DynamicTheme {
-  DynamicTheme();
-
   ThemeData switchTheme(MaterialColor primary) {
     return ThemeData(
       primarySwatch: primary,
